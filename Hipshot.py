0aff66ca7067d16816b7028bba04a04a7a8f1e50
Mansour Moufid <mansourmoufid@gmail.com>
2013-05-15 17:44:25 -0400

    Add a leading underscore to non-public methods and variables.

--- Hipshot.py
+++ Hipshot.py
@@ -3,60 +3,63 @@
 
 import cv
 try:
-    from os import EX_DATAERR, EX_NOINPUT, EX_USAGE
+    from os import EX_DATAERR as _EX_DATAERR
+    from os import EX_NOINPUT as _EX_NOINPUT
+    from os import EX_USAGE as _EX_USAGE
 except ImportError:
-    EX_DATAERR, EX_NOINPUT, EX_USAGE = 1, 2, 3
+    _EX_DATAERR, _EX_NOINPUT, _EX_USAGE = 1, 2, 3
 from os.path import basename, exists, splitext
 from random import randint
-from sys import argv, exit, float_info, stderr
+from sys import argv as _argv, exit as _exit, stderr as _stderr
+from sys import float_info as _float_info
 
-def template_image(image, depth):
+def _template_image(image, depth):
     size = (image.width, image.height)
     channels = image.nChannels
     dup = cv.CreateImage(size, depth, channels)
     return dup
 
-def save_image(image, file):
+def _save_image(image, file):
     while True:
         newfile = splitext(file)[0] + '-'
         newfile = newfile + str(randint(0,1000)) + '.png'
         if not exists(newfile):
             break
-    newimage = template_image(image, cv.IPL_DEPTH_8U)
+    newimage = _template_image(image, cv.IPL_DEPTH_8U)
     cv.ConvertScaleAbs(image, newimage, scale = 255)
     cv.SaveImage(newfile, newimage)
     return
 
-def fail(code):
-    print>>stderr, 'usage: Hipshot.py <file> [alpha]'
-    exit(code)
+def _fail(code):
+    print>>_stderr, 'usage: Hipshot.py <file> [alpha]'
+    _exit(code)
 
 if '__main__' in __name__:
-    if len(argv) == 2:
-        file = argv[1]
+    if len(_argv) == 2:
+        file = _argv[1]
         if not exists(file):
-            exit(EX_NOINPUT)
-    elif len(argv) == 3:
-        file = argv[1]
+            _exit(_EX_NOINPUT)
+    elif len(_argv) == 3:
+        file = _argv[1]
         if not exists(file):
-            exit(EX_NOINPUT)
+            _exit(_EX_NOINPUT)
         try:
-            alpha = float(argv[2])
+            alpha = float(_argv[2])
         except ValueError:
-            fail(EX_USAGE)
-        if alpha <= 10.0 * float_info.epsilon or \
-            alpha >= 1.0 - 10.0 * float_info.epsilon:
-            fail(EX_USAGE)
+            _fail(_EX_USAGE)
+        if alpha <= 10.0 * _float_info.epsilon or \
+            alpha >= 1.0 - 10.0 * _float_info.epsilon:
+            _fail(_EX_USAGE)
     else:
-        fail(EX_USAGE)
+        _fail(_EX_USAGE)
 
     cap = cv.CaptureFromFile(file)
     if not cap:
-        exit(EX_DATAERR)
+        _exit(_EX_DATAERR)
     img = cv.QueryFrame(cap)
     if not img:
-        exit(EX_DATAERR)
-    acc = template_image(img, cv.IPL_DEPTH_32F)
+        _exit(_EX_DATAERR)
+    acc = _template_image(img, cv.IPL_DEPTH_32F)
 
     cv.NamedWindow('Hipshot', flags = cv.CV_WINDOW_AUTOSIZE)
 
@@ -74,10 +77,10 @@ if '__main__' in __name__:
         elif k == ord('z'):
             cv.SetZero(acc)
         elif k == ord('s'):
-            save_image(acc, file)
+            _save_image(acc, file)
         img = cv.QueryFrame(cap)
         if not img:
             break
         cv.RunningAvg(img, acc, alpha)
 
-    save_image(acc, file)
+    _save_image(acc, file)
