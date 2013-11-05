3e36ab9daff1c66a73ce6d326a0214e8ca45ca32
Mansour Moufid <mansourmoufid@gmail.com>
2013-11-04 23:07:15 -0500

    Move OpenCV code to the file 'cvutils.py'.

--- Hipshot.py
+++ Hipshot.py
@@ -14,10 +14,11 @@ try:
     from os import EX_USAGE as _EX_USAGE
 except ImportError:
     _EX_DATAERR, _EX_NOINPUT, _EX_USAGE = 1, 2, 3
-from os.path import exists, splitext
-from random import randint
+from os.path import exists
 from sys import argv as _argv, exit as _exit, stderr as _stderr
 from sys import float_info as _float_info
+
+import cvutils
 
 
 __author__ = 'Mansour Moufid'
@@ -28,54 +29,9 @@ __email__ = 'mansourmoufid@gmail.com'
 __status__ = 'Development'
 
 
-def _template_image(image, depth):
-    size = (image.width, image.height)
-    channels = image.nChannels
-    dup = cv.CreateImage(size, depth, channels)
-    return dup
-
-
-def _save_image(image, file):
-    while True:
-        newfile = splitext(file)[0] + '-'
-        newfile = newfile + str(randint(0, 1000)) + '.png'
-        if not exists(newfile):
-            break
-    newimage = _template_image(image, cv.IPL_DEPTH_8U)
-    cv.ConvertScaleAbs(image, newimage, scale=255)
-    cv.SaveImage(newfile, newimage)
-    print newfile
-    return
-
-
 def _fail(code):
     print>>_stderr, 'usage: Hipshot.py <file> [alpha]'
     _exit(code)
-
-
-def num_frames(video_file):
-    '''Return the number of frames in a video file.
-    '''
-    cap = cv.CaptureFromFile(video_file)
-    if not cap:
-        raise IOError(video_file)
-    n = cv.GetCaptureProperty(cap, cv.CV_CAP_PROP_FRAME_COUNT)
-    return int(n)
-
-
-def get_frames(video_file):
-    '''Return a list of individual frames in a video file.
-    '''
-    cap = cv.CaptureFromFile(video_file)
-    if not cap:
-        raise IOError(video_file)
-    i = 0
-    while i < num_frames(video_file):
-        img = cv.QueryFrame(cap)
-        if not img:
-            break
-        yield img
-        i += 1
 
 
 def merge(frames, alpha, display=None):
@@ -85,7 +41,7 @@ def merge(frames, alpha, display=None):
     acc = None
     for frame in frames:
         if not acc:
-            acc = _template_image(frame, cv.IPL_DEPTH_32F)
+            acc = cvutils._template_image(frame, cv.IPL_DEPTH_32F)
         cv.RunningAvg(frame, acc, alpha)
         if display:
             cv.ShowImage(display, acc)
@@ -95,7 +51,7 @@ def merge(frames, alpha, display=None):
             elif k == ord('z'):
                 cv.SetZero(acc)
             elif k == ord('s'):
-                _save_image(acc, file)
+                print cvutils._save_image(acc, file, random=True)
     return acc
 
 
@@ -121,12 +77,12 @@ if '__main__' in __name__:
     try:
         alpha
     except NameError:
-        alpha = 5e-3 / num_frames(file)
+        alpha = 5e-3 / cvutils.num_frames(file)
 
     try:
-        frames = get_frames(file)
+        frames = cvutils.get_frames(file, as_array=False)
         cv.NamedWindow('Hipshot', flags=cv.CV_WINDOW_AUTOSIZE)
         long_exp_img = merge(frames, alpha, display='Hipshot')
-        _save_image(long_exp_img, file)
+        print cvutils._save_image(long_exp_img, file, random=True)
     except IOError:
         _exit(_EX_DATAERR)
