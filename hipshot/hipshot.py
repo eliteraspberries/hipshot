93fbe76a7a736a4e6304ec51151e8b2b43b719fe
Mansour Moufid <mansourmoufid@gmail.com>
2013-11-05 19:45:10 -0500

    Bump to version 0.3.

--- hipshot/hipshot.py
+++ hipshot/hipshot.py
@@ -7,7 +7,7 @@ simulating a long-exposure photograph.
 __author__ = 'Mansour Moufid'
 __copyright__ = 'Copyright 2013, Mansour Moufid'
 __license__ = 'ISC'
-__version__ = '0.2'
+__version__ = '0.3'
 __email__ = 'mansourmoufid@gmail.com'
 __status__ = 'Development'
 
